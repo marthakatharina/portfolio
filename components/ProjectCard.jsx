7da3f0fb95d32f4ba96ec371193d62f69b127674
Marta Wlusek <wlusek.marta@gmail.com>
2023-12-05 10:53:28 +0100

    conditional rendering for featured image in cards

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -7,35 +7,46 @@ export default function ProjectCard({ project }) {
     const [featuredImage, setFeaturedImage] = useState("");
 
     const getImage = () => {
-        axios
-            .get(project?._links["wp:featuredmedia"][0]?.href)
-            .then((response) => {
-                setFeaturedImage(response.data.source_url);
-            });
+        if (project.featured_media !== 0) {
+            axios
+                .get(project?._links["wp:featuredmedia"][0]?.href)
+                .then((response) => {
+                    setFeaturedImage(response.data.source_url);
+                })
+                .catch((error) => {
+                    console.error("Error fetching image:", error);
+                });
+        } else {
+            // No featured image available
+            setFeaturedImage("");
+        }
     };
 
     useEffect(() => {
         getImage();
-    }, []);
+    }, [project.featured_media]);
 
     return (
         <div className="container">
             <div className="post-card--container">
                 <h2 className="post-card--title">{project.title.rendered}</h2>
-
                 <p
                     className="post-card--excerpt"
                     dangerouslySetInnerHTML={{
                         __html: project.excerpt.rendered,
                     }}
                 />
-
                 <Link to={`/${project.slug}`}>View Project</Link>
-                <img
-                    src={featuredImage}
-                    className="post-card--img"
-                    alt="Featured"
-                />
+
+                {featuredImage !== "" ? (
+                    <img
+                        src={featuredImage}
+                        className="post-card--img"
+                        alt="Featured"
+                    />
+                ) : (
+                    ""
+                )}
             </div>
         </div>
     );

c6f653a66393cf769fae98cd88b28f81037ec4b2
Marta Wlusek <wlusek.marta@gmail.com>
2024-02-24 08:31:32 +0100

    third theme switch added

--- components/ThemeSwitcher.jsx
+++ components/ThemeSwitcher.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import Context from "./Context";
 
-const themes = ["light", "dark"];
+const themes = ["light", "dark", "surprise"];
 
 export default function ThemeSwitcher() {
     const { theme, setTheme } = useContext(Context);

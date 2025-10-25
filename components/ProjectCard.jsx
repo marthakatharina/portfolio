cae7bbe8a6304416ce3c728f907b65bffc461325
Marta Wlusek <wlusek.marta@gmail.com>
2025-10-25 22:04:45 +0200

    featured image changed to webp

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -9,11 +9,9 @@ export default function ProjectCard({ project }) {
                 {project?._links?.featuredmedia?.[0]?.href ? (
                     <Link to={`/${project.slug}`}>
                         <img
-                            src={project._links.featuredmedia[0].href}
+                            src={`${project._links.featuredmedia[0].href}?fm=webp`}
                             className="project-card--img"
                             alt={project.title.rendered}
-                            loading="lazy"
-                            decoding="async"
                         />
                     </Link>
                 ) : (

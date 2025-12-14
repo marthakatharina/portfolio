3afae0cf3f9508d426267a56289b113d9d72d984
Marta Wlusek <wlusek.marta@gmail.com>
2025-12-14 09:34:08 +0100

    star shape changed

--- components/About.jsx
+++ components/About.jsx
@@ -29,15 +29,23 @@ export default function About() {
                         }}
                     >
                         <svg
-                            width="20"
-                            height="20"
+                            width="26"
+                            height="26"
                             viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg"
                             focusable="false"
                         >
                             <path
                                 fill="#ffce00"
-                                d="M12 .587l3.668 7.431 8.2 1.192-5.934 5.79 1.402 8.174L12 18.897 4.664 23.174l1.402-8.174L.132 9.21l8.2-1.192z"
+                                d="M12 2
+           L14.5 9.5
+           L22 12
+           L14.5 14.5
+           L12 22
+           L9.5 14.5
+           L2 12
+           L9.5 9.5
+           Z"
                             />
                         </svg>
                     </div>
